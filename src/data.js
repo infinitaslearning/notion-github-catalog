47d6661b502c85b4fd16e841e7d5292b11bf31b3
cliftonc <clifton.cunningham@gmail.com>
2022-01-28 14:33:57 +0100

    Fix for reference tables having more than 100 entries

--- src/data.js
+++ src/data.js
@@ -3,42 +3,49 @@ const core = require('@actions/core')
 const loadData = async ({ notion }) => {
   const systemDb = core.getInput('system_database')
   const ownerDb = core.getInput('owner_database')
-  const database = core.getInput('database')
+  const serviceDb = core.getInput('database')
 
-  const processRows = (data) => {
-    const parent = {}
-    data.results.forEach((row) => {
-      const name = row.properties.Name?.title[0]?.plain_text?.toLowerCase()
-      if (name) parent[name] = row.id
-    })
-    return parent
+  const getDatabaseRows = async (databaseId, rowFunction, startCursor) => {
+    try {
+      const pageRows = await notion.databases.query({
+        database_id: databaseId,
+        start_cursor: startCursor
+      })
+      pageRows.results.forEach(rowFunction)
+      if (pageRows.has_more) {
+        return await getDatabaseRows(databaseId, rowFunction, pageRows.next_cursor)
+      }
+    } catch (ex) {
+      error = true
+      core.error(`Failed to retrieve data: ${ex.message}`)
+    }
   }
 
   // Get core DB structure
   const dbStructure = await notion.databases.retrieve({
-    database_id: database
+    database_id: serviceDb
   })
   const structure = Object.keys(dbStructure.properties).map((property) => {
     return { name: dbStructure.properties[property].name, type: dbStructure.properties[property].type }
   })
 
   // Get the system and owner db
-  let systemRows, ownerRows
-
+  const systems = {}
   if (systemDb) {
-    systemRows = await notion.databases.query({
-      database_id: systemDb
+    await getDatabaseRows(systemDb, (row) => {
+      const name = row.properties.Name?.title[0]?.plain_text?.toLowerCase()
+      if (name) systems[name] = row.id
     })
   }
 
+  const owners = {}
   if (ownerDb) {
-    ownerRows = await notion.databases.query({
-      database_id: ownerDb
+    await getDatabaseRows(ownerDb, (row) => {
+      const name = row.properties.Name?.title[0]?.plain_text?.toLowerCase()
+      if (name) owners[name] = row.id
     })
   }
 
-  const systems = processRows(systemRows) || null
-  const owners = processRows(ownerRows) || null
   let error = false
 
   if (ownerDb && !owners.unknown) {
@@ -52,22 +59,12 @@ const loadData = async ({ notion }) => {
 
   // Get the current service matrix and hashes in bulk to speed up updates
   const services = {}
-  const getDatabaseRows = async (startCursor) => {
-    const pageRows = await notion.databases.query({
-      database_id: database,
-      start_cursor: startCursor
-    })
-    pageRows.results.forEach((item) => {
-      const pageId = item.id
-      const pageHash = item.properties?.Hash?.rich_text[0]?.text?.content
-      const pageName = item.properties?.Name?.title[0]?.text?.content
-      services[pageName] = { pageId, pageHash }
-    })
-    if (pageRows.has_more) {
-      return await getDatabaseRows(pageRows.next_cursor)
-    }
-  }
-  await getDatabaseRows()
+  await getDatabaseRows(serviceDb, (item) => {
+    const pageId = item.id
+    const pageHash = item.properties?.Hash?.rich_text[0]?.text?.content
+    const pageName = item.properties?.Name?.title[0]?.text?.content
+    services[pageName] = { pageId, pageHash }
+  })
 
   if (error) {
     process.exit(1)
